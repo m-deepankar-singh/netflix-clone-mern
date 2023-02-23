55fdbbe6e4837a18f080ef2143537b24917e7462
Deepankar Singh <singh.deepankar39@gmail.com>
2023-02-23 22:36:20 +0530

    changes

--- netflix-ui/src/store/index.js
+++ netflix-ui/src/store/index.js
@@ -16,7 +16,7 @@ export const getGenres = createAsyncThunk("netflix/genres", async () => {
   const {
     data: { genres },
   } = await axios.get(
-    "https://api.themoviedb.org/3/genre/movie/list?api_key=3d39d6bfe362592e6aa293f01fbcf9b9"
+    "https://api.themoviedb.org/3/genre/movie/list?api_key=13586f004a1e7eac1e04ae12e5e2a4ca"
   );
   return genres;
 });
@@ -56,7 +56,7 @@ export const fetchDataByGenre = createAsyncThunk(
       netflix: { genres },
     } = thunkAPI.getState();
     return getRawData(
-      `https://api.themoviedb.org/3/discover/${type}?api_key=3d39d6bfe362592e6aa293f01fbcf9b9&with_genres=${genre}`,
+      `https://api.themoviedb.org/3/discover/${type}?api_key=13586f004a1e7eac1e04ae12e5e2a4ca&with_genres=${genre}`,
       genres
     );
   }
